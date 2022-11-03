58e4c2143c7c978126effe896b386ea753862d2c
Ajit Kumar <83500463+Ajit-91@users.noreply.github.com>
2022-11-03 21:30:01 +0530

    Time: 43 ms (13.35%), Space: 21.4 MB (77.26%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -11,9 +11,13 @@
  */
 class Solution {
     TreeNode* helper(int low, int high, vector<int>& nums) {
+        // if at any stage low exceeds high then it is out of bound so return
         if(low > high) return NULL;
+        // find mid element in given range low to high
         int mid = low + (high-low)/2;
+        // create new node with mid element
         TreeNode* node = new TreeNode(nums[mid]);
+        // recursively create the left and right of tree
         node->left = helper(low, mid-1, nums);
         node->right = helper(mid+1, high, nums);
         return node;
