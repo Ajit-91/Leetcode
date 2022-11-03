362433486eda16a63721aa5dabc2454d8b6f1b62
Ajit Kumar <83500463+Ajit-91@users.noreply.github.com>
2022-11-03 21:47:31 +0530

    Time: 21 ms (61.39%), Space: 7.9 MB (80.94%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        // Based on fact that if cyle exists then it is sure that fast would meet slow
+        // once, i.e slow == fast
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) return true;
+        }
+        return false;
+    }
+};
