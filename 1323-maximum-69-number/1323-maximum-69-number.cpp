c179e85573c5c3639cda923f8c1fd321de29d11d
Ajit Kumar <83500463+Ajit-91@users.noreply.github.com>
2022-11-07 21:00:32 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (17.66%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,34 +1,17 @@
 class Solution {
-    int reverseNum(int num){
-        int ans = 0;
-        while(num > 0){
-            ans = ans* 10 + (num % 10);
-            num /= 10;
-        }
-        return ans;
-    }
-    string toString(int num){
-        int n = reverseNum(num);
-        string ans;
-        while(n > 0){
-            ans.push_back('0' + (n % 10));
-            n /= 10;
-        }
-        return ans;
-    }
+
 public:
     int maximum69Number (int num) {
-        string n = toString(num);
-        int ans = 0;
-        for(char &c : n){
-            if(c == '6'){
-                c = '9';
-                break;
+        int temp = num;
+        int sixIndex = -1;
+        int i = 0;
+        while(temp > 0){
+            if(temp % 10 == 6){
+                sixIndex = max(sixIndex, i);
             }
+            i++;
+            temp /= 10;
         }
-        for(char c : n){
-            ans = ans*10 + c-'0';
-        }
-        return ans;
+        return num + pow(10, sixIndex) * 3;
     }
 };
