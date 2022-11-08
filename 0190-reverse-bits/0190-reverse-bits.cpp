290fdd050ccce16864124663f1fc53cfbda0842a
Ajit Kumar <83500463+Ajit-91@users.noreply.github.com>
2022-11-08 12:20:48 +0530

    Time: 7 ms (33.76%), Space: 6.2 MB (10.05%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
+        // vector to store reversed bits
         vector<int> bits(32, 0);
         uint32_t ans = 0;
         int i = 0;
         while(n){
+            // if LSB is 1 => push it into bits vector
             if(n & 1){
                 bits[i] = 1;
             }else{
@@ -13,9 +15,11 @@ public:
             n >>= 1;
             i++;
         }
+        // converting binary bits to decimal
         for(int bit : bits){
-            //cout<<bit;
-            ans = ans * 2 + bit;
+        // following is same as ans = ans * 2 + bit but faster
+            ans <<= 1;
+            ans += bit;
         }
         return ans;
     }
