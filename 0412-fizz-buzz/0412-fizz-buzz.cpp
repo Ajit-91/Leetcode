446a93d5d3968dd8a9c515310617ba6a42ce9bc4
Ajit Kumar <83500463+Ajit-91@users.noreply.github.com>
2022-11-14 18:25:48 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (78.06%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -3,15 +3,16 @@ public:
     vector<string> fizzBuzz(int n) {
         vector<string> ans(n);
         for(int i = 0; i < n; i++){
-            if((i+1) % 15 == 0){
-                ans[i] = "FizzBuzz";
-            }else if((i+1) % 3 == 0){
-                ans[i] = "Fizz";
-            }else if((i+1) % 5 == 0){
-                ans[i] = "Buzz";
-            }else{
-                ans[i] = to_string(i+1);
-            }
+            ans[i] = to_string(i+1);
+        }
+        for(int i = 2; i < n; i+=3){
+            ans[i] = "Fizz";
+        }
+        for(int i = 4; i < n; i+=5){
+            ans[i] = "Buzz";
+        }
+        for(int i = 14; i < n; i+=15){
+            ans[i] = "FizzBuzz";
         }
         return ans;
     }
