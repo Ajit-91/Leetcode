cb5b617e292d289150a5d65206aaa193a0a09f90
Ajit Kumar <83500463+Ajit-91@users.noreply.github.com>
2022-12-11 21:12:10 +0530

    Time: 0 ms (100.00%), Space: 6 MB (32.37%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+int reverse(int x)
+{
+    int ans = 0;
+    while (x != 0)
+    {
+        int unitDigit = x % 10;
+        // overflow conditions
+        if(x > 0 && ans > (INT_MAX - unitDigit)/10)
+        {
+            return 0;
+        }
+        if(x < 0 && ans < (INT_MIN - unitDigit)/10){
+            return 0;
+        }
+        ans = (ans*10) + unitDigit;
+        x /= 10;
+    }
+    return ans;
+}
+};
