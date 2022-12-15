3cd94e349e7dc9358a4c68406d295861bbe999b5
Ajit Kumar <83500463+Ajit-91@users.noreply.github.com>
2022-12-15 22:40:57 +0530

    Added solution - LeetHub

--- Second Largest - GFG/second-largest.cpp
+++ Second Largest - GFG/second-largest.cpp
@@ -11,22 +11,44 @@ public:
 	// largest elements
 	int print2largest(int arr[], int n) {
 	    // code here
-	    if(n < 2) return -1;
-	    // find largest elm
-        int largest1 = INT_MIN;
-        for(int i = 0; i < n; i++)
-        {
-            largest1 = max(arr[i], largest1);
-        }
+	    
+	    //-------------O(2N)-----------------
+	   // if(n < 2) return -1;
+	   // // find largest elm
+    //     int largest1 = INT_MIN;
+    //     for(int i = 0; i < n; i++)
+    //     {
+    //         largest1 = max(arr[i], largest1);
+    //     }
         
-        // 2nd largest would be the largest elm among all except largest1
-        int largest2 = INT_MIN;
+    //     // 2nd largest would be the largest elm among all except largest1
+    //     int largest2 = INT_MIN;
+    //     for(int i = 0; i < n; i++)
+    //     {
+    //         if(arr[i] == largest1) continue;
+    //         largest2 = max(arr[i], largest2);
+    //     }
+    //     return largest2 == INT_MIN ? -1 : largest2;
+    
+    
+    //--------------O(N)-------------------------
+    
+        int first = INT_MIN;
+        int second = INT_MIN;
+        
         for(int i = 0; i < n; i++)
         {
-            if(arr[i] == largest1) continue;
-            largest2 = max(arr[i], largest2);
+            if(arr[i] > first)
+            {
+                second = first;
+                first = arr[i];
+            }
+            else if(arr[i] > second && arr[i] < first)
+            {
+                second = arr[i];
+            }
         }
-        return largest2 == INT_MIN ? -1 : largest2;
+        return second == INT_MIN ? -1 : second;
 	}
 };
 
