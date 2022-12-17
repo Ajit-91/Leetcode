683f675605a9cdcf5c23e55cf6912ef261ee2740
Ajit Kumar <83500463+Ajit-91@users.noreply.github.com>
2022-12-17 21:42:46 +0530

    Added solution - LeetHub

--- Quick Left Rotation - GFG/quick-left-rotation.cpp
+++ Quick Left Rotation - GFG/quick-left-rotation.cpp
@@ -5,29 +5,50 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 	
-	
+	void reverseArray(int arr[], int n)
+	{
+	    int start = 0;
+	    int end = n - 1;
+	    while(start < end)
+	    {
+	        swap(arr[start++], arr[end++]);
+	    }
+	}
 	public:
 	void leftRotate(int arr[], int k, int n) 
 	{ 
 	   // Your code goes here
+	   
+	   //-----Approach 1 : Extra space-----------------
+	   //k = k % n;
+	   //if(k == 0) return;
+	   //int temp [k];
+	   //// copy first k elements in temp
+	   //for(int  i = 0; i < k; i++)
+	   //{
+	   //    temp[i] = arr[i];
+	   //}
+	   //// shift(here left) elements by k in original arr
+	   //for(int i = k; i < n; i++)
+	   //{
+	   //    arr[i-k] = arr[i];
+	   //}
+	   //// copy contents of temp into original arr
+	   //for(int i = 0; i < k; i++)
+	   //{
+	   //    arr[n-k+i] = temp[i];
+	   //}
+	   
+	   //-------Approach : 2-----------------------------
 	   k = k % n;
 	   if(k == 0) return;
-	   int temp [k];
-	   // copy first k elements in temp
-	   for(int  i = 0; i < k; i++)
-	   {
-	       temp[i] = arr[i];
-	   }
-	   // shift(here left) elements by k in original arr
-	   for(int i = k; i < n; i++)
-	   {
-	       arr[i-k] = arr[i];
-	   }
-	   // copy contents of temp into original arr
-	   for(int i = 0; i < k; i++)
-	   {
-	       arr[n-k+i] = temp[i];
-	   }
+	   //reverseArray(arr, k);
+	   //reverseArray(arr+k, n-k);
+	   //reverseArray(arr, n);
+	   reverse(arr, arr+k);
+	   reverse(arr+k, arr+n);
+	   reverse(arr, arr+n);
+	   
 	} 
 		 
 
